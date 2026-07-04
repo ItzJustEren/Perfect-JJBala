265513c246e6ce0084aa93462496317e7039db0b
ItzJustEren <yzmtenta@gmail.com>
2026-07-04 22:20:30 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
+import asyncio
+import os
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from database import init_db
+from handlers import dp, auto_send_ideas
 
+load_dotenv()
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+async def main():
+    init_db()
+    asyncio.create_task(auto_send_ideas())
+    await dp.start_polling(Bot(token=TOKEN))
+
+if __name__ == "__main__":
+    asyncio.run(main())
